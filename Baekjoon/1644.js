f59d1eb1dd111f91e364bba7fdebb4e0a316812c
dongjji <y2kdj9723@naver.com>
2022-03-11 12:08:48 +0900

    Day283 BOJ 1644 소수의 연속합 리팩토링

--- Baekjoon/1644.js
+++ Baekjoon/1644.js
@@ -6,16 +6,33 @@ function isPrime(num) {
   return true;
 }
 
+function primeList(num) {
+  const list = [];
+  const primeOrNot = Array(num + 1).fill(true);
+  for (let i = 2; i ** 2 <= num; i++) {
+    for (let j = i ** 2; j <= num; j += i) {
+      primeOrNot[j] = false;
+    }
+  }
+
+  for (let i = 2; i < primeOrNot.length; i++) {
+    if (primeOrNot[i]) list.push(i);
+  }
+  return list;
+}
+
 const fs = require("fs");
 const num = +fs.readFileSync("input.txt").toString();
-let arr = [2];
-let sum = 9;
-for (let i = 2; i <= num; i++) {
-  if (isPrime(i)) {
-    sum += i;
-    arr.push(i);
-  }
-}
+let arr = primeList(num);
+console.log(arr);
+// let arr = [2];
+// let sum = 9;
+// for (let i = 2; i <= num; i++) {
+//   if (isPrime(i)) {
+//     sum += i;
+//     arr.push(i);
+//   }
+// }
 
 let arr2 = Array(num + 1);
 for (let i = 0; i < arr.length; i++) {
@@ -47,7 +64,7 @@ const rl = readline.createInterface({
 });
 
 const input = [];
-const generatePrime = value => {
+const generatePrime = (value) => {
   const primes = [];
   const check = new Array(value + 1).fill(true);
   for (let i = 2; i * i <= value; i++) {
@@ -75,7 +92,10 @@ rl.on("line", function (line) {
 
   while (left <= right) {
     if (left === primeList.length || right === primeList.length) break;
-
+    // 투 포인터: sum값이 num값보다 크면 왼쪽부터 하나씩 지워나가는 방법. 지운 후에 left 증가 (이게 문제가 요구한 진짜 답인듯.)
+    // sum값이 num보다 작으면 오른쪽을 늘려서 더하는 방법. right를 증가 후 더함.
+    // sum값이 num과 같으면 count를 증가시킴
+    // 나와 다른 점: 나는 그 수까지 경우의 수를 다 구했지만 이 방법은 num이라는 숫자의 경우만 구함.(이것이 시간 단축의 포인트)
     if (sum < target) {
       sum += primeList[++right];
     } else if (sum > target) {
