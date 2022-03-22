0e7aed2161af883f5b3f7063d2330b1d81772801
dongjji <y2kdj9723@naver.com>
2022-03-22 21:19:14 +0900

    [DataStructure-JS] Graph Implementation

--- js/DataStructure/data_structure/graph.js
+++ js/DataStructure/data_structure/graph.js
@@ -1,28 +1,34 @@
-const { resourceLimits } = require("worker_threads");
-
 class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
   addEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1].push(vertex2)
-      this.adjacencyList[vertex2].push(vertex1)
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
     } else {
       return undefined;
     }
   }
+
   removeEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2)
-      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        v => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        v => v !== vertex1
+      );
     } else {
       return undefined;
     }
   }
+
   removeVertex(vertex) {
     if (this.adjacencyList[vertex]) {
       while (this.adjacencyList[vertex].length) {
@@ -33,6 +39,7 @@ class Graph {
       return undefined;
     }
   }
+
   DFS(start) {
     const result = [];
     const visited = {};
@@ -41,17 +48,18 @@ class Graph {
     function dfs(vertex) {
       if (!vertex) return null;
       visited[vertex] = true;
-      result.push(vertex)
-      adjacencyList[vertex].forEach((neighbor) => {
-        if(!visited[neighbor]) {
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
           dfs(neighbor);
         }
-      })
+      });
     }
 
-    dfs(start)
-    return result
+    dfs(start);
+    return result;
   }
+
   DFS_iterative(start) {
     const stack = [start];
     const visited = {};
@@ -69,10 +77,11 @@ class Graph {
           visited[neighbor] = true;
           stack.push(neighbor);
         }
-      })
+      });
     }
     return result;
   }
+
   BFS(start) {
     const queue = [start];
     const visited = {};
@@ -80,36 +89,36 @@ class Graph {
 
     let currentVertex;
     const result = [];
-    
+
     while (queue.length) {
       currentVertex = queue.shift();
       result.push(currentVertex);
 
       this.adjacencyList[currentVertex].forEach(neighbor => {
-        if(!visited[neighbor]) {
+        if (!visited[neighbor]) {
           visited[neighbor] = true;
-          queue.push(neighbor)
+          queue.push(neighbor);
         }
-      })
+      });
     }
     return result;
   }
 }
 
 const g = new Graph();
-g.addVertex("A")
-g.addVertex("B")
-g.addVertex("C")
-g.addVertex("D")
-g.addVertex("E")
-g.addVertex("F")
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
-g.addEdge("A", "B")
-g.addEdge("A", "C")
-g.addEdge("B", "D")
-g.addEdge("C", "E")
-g.addEdge("D", "E")
-g.addEdge("D", "F")
-g.addEdge("E", "F")
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
 
-console.log(g.BFS("A"))
+console.log(g.BFS("A"));
