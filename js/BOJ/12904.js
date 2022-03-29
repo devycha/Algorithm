b387e0764fa8c493ea568675d3dbdccaff158ff0
dongjji <y2kdj9723@naver.com>
2022-03-29 13:15:46 +0900

    fix: [BOJ-12904-JS] A와 B(주석설명 편집)

--- js/BOJ/12904.js
+++ js/BOJ/12904.js
@@ -17,6 +17,7 @@
 const fs = require("fs");
 let [S, T] = fs.readFileSync("input.txt").toString().split("\n");
 
+// 문자열 뒤집기 함수
 function reverseString(s) {
   let str = "";
   for (let i = s.length - 1; i >= 0; i--) {
@@ -25,13 +26,20 @@ function reverseString(s) {
   return str;
 }
 
+/**
+ * 두가지 연산에 대해서 T를 거꾸로 지워나간 뒤에
+ * S의 길이와 T의 길이가 같아질 때 비교하는 방법을 사용
+ */
 while (S.length < T.length) {
+  // 맨 끝자리가 A로 끝나면 맨 끝의 A를 지움
   if (T[T.length - 1] === "A") {
     T = T.substring(0, T.length - 1);
   } else {
+    // 맨 끝자리가 B로 끝나면 맨 끝의 B를 지우고 문자열을 뒤집음
     T = reverseString(T.substring(0, T.length - 1));
   }
 }
+// S와 T의 길이가 같을 때 두 값이 같으면 1 다르면 0을 출력
 console.log(S === T ? 1 : 0);
 /**
  * 채점 결과
