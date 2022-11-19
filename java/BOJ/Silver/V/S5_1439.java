5566fe87b94645b4024757202d1adf7016887ef1
dongjji <y2kdj9723@naver.com>
2022-11-19 12:17:31 +0900

    [BOJ-1439-JAVA] 뒤집기

---
+++ java/BOJ/Silver/V/S5_1439.java
@@ -0,0 +1,55 @@
+/**
+ * ? 문제 출처: 백준 온라인 져지(BOJ)
+ * ? 시간 제한: 2초
+ * ? 메모리 제한: 128MB
+ *
+ * ? 문제
+ * 다솜이는 0과 1로만 이루어진 문자열 S를 가지고 있다. 다솜이는 이 문자열 S에 있는 모든 숫자를 전부 같게 만들려고 한다. 다솜이가 할 수 있는 행동은 S에서 연속된 하나 이상의 숫자를 잡고 모두 뒤집는 것이다. 뒤집는 것은 1을 0으로, 0을 1로 바꾸는 것을 의미한다.
+ * 예를 들어 S=0001100 일 때,
+ * 전체를 뒤집으면 1110011이 된다.
+ * 4번째 문자부터 5번째 문자까지 뒤집으면 1111111이 되어서 2번 만에 모두 같은 숫자로 만들 수 있다.
+ * 하지만, 처음부터 4번째 문자부터 5번째 문자까지 문자를 뒤집으면 한 번에 0000000이 되어서 1번 만에 모두 같은 숫자로 만들 수 있다.
+ * 문자열 S가 주어졌을 때, 다솜이가 해야하는 행동의 최소 횟수를 출력하시오.
+ *
+ * ? 입력 & 파싱
+ * 첫째 줄에 문자열 S가 주어진다. S의 길이는 100만보다 작다.
+ *
+ * 0001100  -> arr
+ *
+ * ? 출력
+ * 첫째 줄에 다솜이가 해야하는 행동의 최소 횟수를 출력한다.
+ *
+ * 1
+ *
+ * ? 채점 결과
+ * * 시간: 0.076ms
+ * * 메모리: 11MB
+ * * 언어: JAVA8
+ * * 시도: 1
+ */
+package Silver.V;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class S5_1439 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        char[] arr = br.readLine().toCharArray();
+        int[] count = new int[2];
+
+        int before = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int cur = arr[i] - '0';
+            if (cur != before) {
+                before = cur;
+                count[cur]++;
+            }
+        }
+
+        System.out.println(Math.min(count[0], count[1]));
+    }
+}
